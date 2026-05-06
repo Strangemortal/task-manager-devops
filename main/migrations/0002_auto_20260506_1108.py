c89778e59078df6de62be56a3d88e2dbe42ed360
Bharat_Bhushan <bittu979766@gmail.com>
2026-05-06 16:39:18 +0530

    add superuser migration

---
+++ main/migrations/0002_auto_20260506_1108.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+def create_superuser(apps, schema_editor):
+    User = apps.get_model('main', 'User')
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@example.com',
+            password='adminpassword123',
+            role='admin'
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
